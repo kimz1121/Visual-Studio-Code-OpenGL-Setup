0217ca629bab6fe2fd43ada041c6c414df9db0cb
kimz1121 <kimz1121@naver.com>
2021-01-21 15:15:13 +0900

    이미지_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,71 @@
-#include "engine.h"
+#include "../dependencies/GLAD/include/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+// #include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// ��ũ�� ũ��
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-    Engine engine(640, 480, "OpenGL preset by Guylian Gilsing");
+    // glfw �ʱ�ȭ
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    if(!engine.Initialize())
+    // window ����
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "songsmir GL", NULL, NULL);
+    if (window == NULL)
     {
-        std::cout << std::endl << "Press any key to close program..." << std::endl;
-        std::cin.get();
+        // std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
     }
+    // ���� �����쿡 context�� ����
+    glfwMakeContextCurrent(window);
+    // buffer�� ũ�Ⱑ ����� �� ȣ��Ǵ� �ݹ�
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad �ε�
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        // std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // ���� ����
+    while (!glfwWindowShouldClose(window))
+    {
+        // �Է� ó��
+        processInput(window);
+
+        // ���� �ʱ�ȭ
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+       
+        // ���� ���
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
 
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
